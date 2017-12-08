444fdefdb8a166ddf78379b7caa588e39475205b
philip ka sing leung <philip.leung90@gmail.com>
2017-12-08 03:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+function happyHolidays(){
+  
+  return "Happy Holidays!";
+  
+}
+
+function happyHolidaysTo(name){
+  
+  return `Happy holidays, ${name}!`;
+  
+}
+
+function happyHolidayTo(name , holiday){
+ 
+ return `Happy ${holiday}, ${name}!`;
+  
+}
+
+function holidayCountdown(holiday , days){
+  
+  return `It is ${days} days until ${holiday}!`;
+  
+}
